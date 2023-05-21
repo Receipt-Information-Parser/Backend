cc03f82e92395435f43f1d5a56287122eaf139c8
Charlieppark <chanho0309@gmail.com>
2023-05-21 16:27:40 +0900

    feat(picture): list, get picture

---
+++ src/main/java/cloudComputing/ReceiptMate/controller/PictureController.java
@@ -0,0 +1,59 @@
+package cloudComputing.ReceiptMate.controller;
+
+import cloudComputing.ReceiptMate.dto.PictureResponse;
+import cloudComputing.ReceiptMate.service.PictureService;
+import com.google.api.client.util.IOUtils;
+import com.jlefebure.spring.boot.minio.MinioException;
+import com.jlefebure.spring.boot.minio.MinioService;
+import io.minio.messages.Item;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/picture")
+public class PictureController {
+
+    @Autowired
+    private final MinioService minioService;
+    private final PictureService pictureService;
+
+    @GetMapping("/list")
+    public List<Item> photoList() throws MinioException {
+        return minioService.list();
+    }
+
+
+    @GetMapping("/{object}")
+    public void getObject(@PathVariable("object") String object, HttpServletResponse response) throws MinioException, IOException {
+        InputStream inputStream = minioService.get(Path.of(object));
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+
+        // Set the content type and attachment header.
+        response.addHeader("Content-disposition", "attachment;filename=" + object);
+        response.setContentType(URLConnection.guessContentTypeFromName(object));
+
+        // Copy the stream to the response's output stream.
+        IOUtils.copy(inputStream, response.getOutputStream());
+        response.flushBuffer();
+    }
+}
+
